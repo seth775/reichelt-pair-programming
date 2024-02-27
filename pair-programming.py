b2a659855755bc402b56074d51a3dc3857e70d2c
Katie Gonzalez <kgonzalez@mines.edu>
2024-02-27 08:38:04 -0700

    Added feedback and test function

--- pair-programming.py
+++ pair-programming.py
@@ -24,4 +24,26 @@ inches = 3
 print(f"{feet} feet and {inches} inches is equal to {convert_to_meter(feet, inches)} meters.")
 
 
+# FEEDBACK - KATIE GONZALEZ
+# code is well-documented and provides useful example documentation for the function
+# code works well with both floats and integers, returning a float 
+# consider adding in-code comments as documentation
 
+# test function
+def test_function():
+    '''
+    Tests Seth's implemented function to convert feet and inches to meters.
+
+    No parameters, returns void.
+
+    Test cases are integer and float cases.
+    '''
+    # Test Case 1: feet = 10, inches = 7
+    result_1 = convert_to_meter(10, 7)
+    assert result_1 == 3.2258, f"Test Case 1 Failed. Expected 3.2258, got {result_1}"
+
+    # Test Case 2: feet = 0, inches = 12.5
+    result_2 = convert_to_meter(0, 12.5)
+    assert result_2 == 0.3175, f"Test Case 2 Failed. Expected 0.3175, got {result_1}"
+
+    print("All test cases passed successfully.")
